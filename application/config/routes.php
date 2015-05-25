36e54d22e7e159481927196cbdbb251fe2a7a6fa
Young Han <yhan.ccs@gmail.com>
2015-05-24 22:57:32 -0700

    Adds Add capability to News

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,6 +58,7 @@ $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 */
 
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
