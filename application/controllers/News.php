0115dc6b8d9e677389bf54b3960c9083f642b62e
Young Han <yhan.ccs@gmail.com>
2015-06-04 11:36:46 -0700

    Cleans up view.php in news

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -1,24 +1,20 @@
 <?php
+//News.php controller
 class News extends CI_Controller {
 
     public function __construct()
-    {
+    {//everything here is global to all methods in the controller
         parent::__construct();
         $this->load->model('news_model');
         $this->config->set_item('banner', 'Global News Banner');
-    }
+    }#end constructor
 
     public function index()
     {
         $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
-
-        //$this->config->set_item('banner', 'News Banner');
-        
-        //$this->load->view('templates/header', $data);
         $this->load->view('news/index', $data);
-        //$this->load->view('templates/footer');
-    }
+    }#end index()
 
     public function view($slug = NULL)
     {
@@ -30,11 +26,8 @@ class News extends CI_Controller {
         }
 
         $data['title'] = $data['news_item']['title'];
-
-        $this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
-        $this->load->view('templates/footer');
-    }
+    }#end view()
     
     public function create()
     
@@ -52,12 +45,13 @@ class News extends CI_Controller {
             $this->load->view('templates/header', $data);
             $this->load->view('news/create', $data);
             $this->load->view('templates/footer', $data);
-        }
         
+        }
         else
         {
             $this->news_model->set_news();
             $this->load->view('news/success');
         }
     }
+
 }
